31e17390086d4c289feccde5f97595a9fc1e653c
Manuel Honoré de Sousa <71514733+manuelhonoredesousa@users.noreply.github.com>
2023-04-07 00:21:24 +0100

    test, stage 1

---
+++ test/dataFile.js
@@ -0,0 +1,61 @@
+const lang = [ 'portuguese', 'english', 'spanish', 'arabic', 'french', 'hindi', 'russian', 'mandarin', 'korean', 'japanese',
+];
+
+function verificarAsKeys(data = Array()) {
+  const allTitles = Object.keys(data);
+  const allTitlesLength = Object.keys(data).length;
+  const problem = [];
+
+  for (let c = 0; c < allTitlesLength; c++) {
+    const title = allTitles[c];
+    const topicQuestions = data[title];
+    topicQuestions.forEach((quiz) => {
+      const keys = Object.keys(quiz);
+
+      keys.forEach((key) => {
+        if (
+          key === 'id' ||
+          key === 'question' ||
+          key === 'options' ||
+          key === 'answer'
+        ) {
+        } else {
+          problem.push({ id: quiz.id, topic: title });
+        }
+      });
+    });
+  }
+  console.log(problem);
+}
+function verificarSeExisteQuatroKeys(data = Array()) {
+  const allTitles = Object.keys(data);
+  const allTitlesLength = Object.keys(data).length;
+  const problem = [];
+
+  for (let c = 0; c < allTitlesLength; c++) {
+    const title = allTitles[c];
+    const topicQuestions = data[title];
+    topicQuestions.forEach((quiz) => {
+      const keysLength = Object.keys(quiz).length;
+      if (keysLength === 4) {
+      } else {
+        problem.push({ id: quiz.id, topic: title });
+      }
+    });
+  }
+  console.log(problem);
+}
+function verificarSeAnswerBateNasOptions(data = Array()) {
+    
+}
+
+
+
+
+lang.forEach(lg => {
+    const dataFile = require('./../src/Functions/QuizGameData/database/'+lg+'_QuizGameSouDEV.json').quiz;
+    console.log("\n>>> ", lg);
+    // verificarAsKeys(dataFile);
+    // verificarSeExisteQuatroKeys(dataFile);
+    
+})

af6aa41bea8dfdf58c981b49ccb8941642846abe
Manuel Honoré de Sousa <71514733+manuelhonoredesousa@users.noreply.github.com>
2023-04-08 00:30:55 +0100

    test, stage 2

--- test/dataFile.js
+++ test/dataFile.js
@@ -46,16 +46,40 @@ function verificarSeExisteQuatroKeys(data = Array()) {
   console.log(problem);
 }
 function verificarSeAnswerBateNasOptions(data = Array()) {
-    
+  const allTitles = Object.keys(data);
+  const allTitlesLength = Object.keys(data).length;
+  const problem = [];
+
+  for (let c = 0; c < allTitlesLength; c++) {
+    const title = allTitles[c];
+    const topicQuestions = data[title];
+    topicQuestions.forEach((quiz) => {
+      
+      const answer = quiz.answer
+      const options = quiz.options
+
+      if (options.includes(answer)) {
+      }else{
+        problem.push({ id: quiz.id, topic: title });
+      }
+
+    });
+  }
+  console.log("Total of errors: " + problem.length);
+  console.log(problem);
 }
 
 
 
-
-lang.forEach(lg => {
-    const dataFile = require('./../src/Functions/QuizGameData/database/'+lg+'_QuizGameSouDEV.json').quiz;
-    console.log("\n>>> ", lg);
-    // verificarAsKeys(dataFile);
-    // verificarSeExisteQuatroKeys(dataFile);
+// lang.forEach(lg => {
+    // const dataFile = require('./../src/Functions/QuizGameData/database/'+lg+'_QuizGameSouDEV.json').quiz;
+    // console.log("\n>>> ", lg);
+    // //verificarAsKeys(dataFile);
+    // //verificarSeExisteQuatroKeys(dataFile);
+    // verificarSeAnswerBateNasOptions(dataFile)
+    // })
     
-})
+    
+    const dataFile = require('./../src/Functions/QuizGameData/database/french_QuizGameSouDEV.json').quiz;
+    
+    verificarSeAnswerBateNasOptions(dataFile)
